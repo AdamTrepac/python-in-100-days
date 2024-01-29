0781dbe3b035abeb68a3324bf271f67ceec7d5b1
adam t <adam.trepac@gmail.com>
2024-01-28 17:48:48 -0800

    add ability to clear calculator via recursion + reduced repeating code

--- Day_10/calculator.py
+++ Day_10/calculator.py
@@ -1,7 +1,5 @@
 from art_calc import logo
 
-
-
 # Add function
 def add(n1, n2):
     return n1 + n2
@@ -18,6 +16,25 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+def calculator():
+    num1 = int(input("What is your first number?: "))
+    # While loop to allow user to continue calculating with the answer
+    while True:
+
+        print(*operations, "which operation would you like to do?", end = " ")
+        operation_symbol = input(": ")
+        num2 = int(input("What is your next number: "))
+
+        function = operations[operation_symbol]
+        answer = function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        num1 = answer
+
+        end = input(f"Type 'y' to continue calculating with {answer} or type 'c' to clear: ").lower()
+        if end == "c":
+            calculator()
+
 operations = {
     "+": add, 
     "-": subtract, 
@@ -25,33 +42,4 @@ operations = {
     "/": divide
     }
 
-
-num1 = int(input("What is your first number?: "))
-print(*operations, "which operation would you like to do?", end = " ")
-operation_symbol = input(": ")
-num2 = int(input("What is your second number: "))
-
-function = operations[operation_symbol]
-answer = function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-# While loop to allow user to continue calculating with the answer
-while True:
-    
-    end = input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit: ").lower()
-    if end == "n":
-        break
-
-    print(*operations, "which operation would you like to do?", end = " ")
-    operation_symbol = input(": ")
-    num3 = int(input("What is your next number: "))
-
-    function = operations[operation_symbol]
-    answer = function(answer, num3)
-
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-    
-
-
+calculator()
