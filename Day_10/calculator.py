83b520d99cd65e2be0e11d6ecb3211b282ac216c
adam t <adam.trepac@gmail.com>
2024-01-28 18:02:33 -0800

    added exponent function, added ability to input floats, modified logo

--- Day_10/calculator.py
+++ Day_10/calculator.py
@@ -16,14 +16,25 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+def exponent(n1, n2):
+    if n1 <= 0:
+        return 1
+    answer = 1
+    for i in range(int(n2)):
+        answer *= n1
+    return answer
+
+
 def calculator():
-    num1 = int(input("What is your first number?: "))
+    print(logo)
+    
+    num1 = float(input("What is your first number?: "))
     # While loop to allow user to continue calculating with the answer
     while True:
 
         print(*operations, "which operation would you like to do?", end = " ")
         operation_symbol = input(": ")
-        num2 = int(input("What is your next number: "))
+        num2 = float(input("What is your next number: "))
 
         function = operations[operation_symbol]
         answer = function(num1, num2)
@@ -31,15 +42,19 @@ def calculator():
         print(f"{num1} {operation_symbol} {num2} = {answer}")
         num1 = answer
 
-        end = input(f"Type 'y' to continue calculating with {answer} or type 'c' to clear: ").lower()
+        end = input(f"Type 'y' to continue calculating with {answer}, type 'c' to clear, or 'q' to quit: ").lower()
         if end == "c":
             calculator()
+        elif end == "q":
+            return
 
 operations = {
     "+": add, 
     "-": subtract, 
     "*": multiply, 
-    "/": divide
+    "/": divide,
+    "^": exponent
     }
 
 calculator()
+print("goodbye")
