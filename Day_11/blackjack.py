db63abed5a0836d2eb9e1e0493467215ba9a1fd2
adam t <adam.trepac@gmail.com>
2024-01-29 23:46:32 -0800

    omg restructred the entire code to add proper high + low aces, took over 2 hours but its way better

--- Day_11/blackjack.py
+++ Day_11/blackjack.py
@@ -29,81 +29,82 @@ from art import logo
 
 
 def new_card(cards, amount):
-    card_list = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1]
-    weights =   [1 , 1, 1, 1, 1, 1, 1, 1, 1, 1 , 1 , 1 , 1 , 0]
-
-    cards += random.choices(card_list, k = amount, weights = weights)
+    for _ in range(amount):
+        cards += [random.randint(0, 12)] # 13 is the number of distinct cards in the deck
     return cards
 
 # lost = check_loss(cards, card_dict) # temp spot for function to check if player won or lost    
 
-def check_loss(cards, card_dict):
-    total_value = 0
-    for card in cards:
-        total_value += card_dict[card]
-    if total_value > 21:
-        return True
-    return False
+# # function to return a true/false value if someone goes over, not implemented
+# def card_overflow(cards, card_dict):
+#     total_value = 0
+#     for card in cards:
+#         total_value += card_dict[card]
+#     if total_value > 21:
+#         return True
+#     return False
 
-def check_value(cards, card_dict):
+def check_value(cards):
+    card_value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1]
     total_value = 0
+
     for card in cards:
-        total_value += card_dict[card]
-    # if total_value > 
+        total_value += card_value[card]
+    if total_value > 21:            # check for high aces to convert to low aces
+        for i in range(len(cards)):
+            if cards[i] == 0:
+                cards[i] = 13
+                total_value = check_value(cards)
+                break
     return total_value
 
 def print_player_cards(cards):
-    card_dict = {11: "A", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10", 10: , 10, 10, 1}
+    card_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
     print("Your cards:", end = " ")
-    for card in cards:
-        print(card, end = " ")
+    for index in cards:
+        print(card_list[index], end = " ")
     print()
 
 def print_dealer_cards(cards):
+    card_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
     print("The dealer's cards:", end = " ")
-    for card in cards:
-        print(card, end = " ")
+    for index in cards:
+        print(card_list[index], end = " ")
     print()
 
-def player_turn(player_cards, card_dict, card_list):
-    game_over = False
-    
+def player_turn(player_cards):
     another_card = input("Type y to get another card, type n to pass: ")
     while another_card == "y":
-        game_over, player_cards = new_card(player_cards, card_dict, card_list)
+        player_cards = new_card(player_cards, 1)
         print_player_cards(player_cards)
-        if game_over == True:
+        if check_value(player_cards) > 21:
             print("you went over, you lost")
-            return game_over, player_cards
+            return []
         another_card = input("Type y to get another card, type n to pass: ")
-    return game_over, player_cards
+    return player_cards
 
-def dealer_turn(player_cards, dealer_cards, card_dict, card_list):
+def dealer_turn(player_cards, dealer_cards):
     # 1. Show both cards
     # 2. If total is >= 17, stand and compare to user
     # 3. If total is < 17, take another card and re-compute
-    game_over = False
 
     print_dealer_cards(dealer_cards) # show both of dealers cards
-    while (check_value(dealer_cards,card_dict)) < 17:
-        game_over, dealer_cards = new_card(dealer_cards, card_dict, card_list)
+    while (check_value(dealer_cards)) < 17:
+        dealer_cards = new_card(dealer_cards, 1)
         print_dealer_cards(dealer_cards)
-        if game_over == True:
+        if check_value(dealer_cards) > 21:
             print("The dealer went over, you won!")
-            return game_over, dealer_cards
+            return
 
-    if check_value(dealer_cards, card_dict) >= check_value(player_cards, card_dict):
+    if check_value(dealer_cards) >= check_value(player_cards):
         print("Your card value does not surpass the dealer, you lose")
-        game_over = True
-        return game_over, dealer_cards
+        return
 
     print("You surpassed the dealers hand, you won!")
-    return game_over, dealer_cards
+    return
     
-
-# card_dict = {"A": 11, "K": 10, "Q": 10, "J": 10, "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3":3, "2":2}
-# card_list = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
-
 
 print(logo)
 
@@ -113,14 +114,13 @@ print_player_cards(player_cards)
 
 dealer_cards = []
 dealer_cards = new_card(dealer_cards, 2)
-print("The first dealer's card: ",dealer_cards[0])
+print_dealer_cards(dealer_cards[:1])
 
 # # Ask user to hit
-game_over, player_cards = player_turn(player_cards, card_dict, card_list)
+player_cards = player_turn(player_cards)
 
-# # Dealer actions:
-# if game_over == False:
-#     dealer_turn(player_cards, dealer_cards, card_dict, card_list)
+# Dealer actions:
+if player_cards:
+    dealer_turn(player_cards, dealer_cards)
 
-# print("Thanks for playing")
-
+print("Thanks for playing")
