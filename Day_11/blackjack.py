e4f2bdfa87cc0e7111d739effbb3a90edb4b8c1f
adam t <adam.trepac@gmail.com>
2024-01-30 22:39:12 -0800

    added blackjake win mode to game, refactored and cleaned up functions

--- Day_11/blackjack.py
+++ Day_11/blackjack.py
@@ -2,6 +2,7 @@
 
 from functools import total_ordering
 import random
+from subprocess import check_call
 from tabnanny import check
 from art import logo
 
@@ -23,26 +24,10 @@ from art import logo
 # 5. Ask if they want to hit again
 # 6. Have dealer play until they have enough cards to beat the user, or break
 
-# Assumptions:
-# 1. Infinite deck - make choosing the cards easier, don't need to track random cards
-# 2. Assume ace = 11 - can make this behavior more complicated later
-
-
 def new_card(cards, amount):
     for _ in range(amount):
         cards += [random.randint(0, 12)] # 13 is the number of distinct cards in the deck
-    return cards
-
-# lost = check_loss(cards, card_dict) # temp spot for function to check if player won or lost    
-
-# # function to return a true/false value if someone goes over, not implemented
-# def card_overflow(cards, card_dict):
-#     total_value = 0
-#     for card in cards:
-#         total_value += card_dict[card]
-#     if total_value > 21:
-#         return True
-#     return False
+    return cards  
 
 def check_value(cards):
     card_value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1]
@@ -58,27 +43,38 @@ def check_value(cards):
                 break
     return total_value
 
-def print_player_cards(cards):
+def check_blackjack(player_cards, dealer_cards):
+    player_blackjack = False
+    dealer_blackjack = False
+    if check_value(player_cards) == 21:
+        player_blackjack = True
+    if check_value(dealer_cards) == 21:
+        dealer_blackjack = True
+    
+    if not player_blackjack and not dealer_blackjack:
+        return False
+    elif player_blackjack and dealer_blackjack: 
+        print("Both the dealer and player have a blackjack, the game ends in a draw")
+    elif player_blackjack:
+        print("The player wins with a blackjack!")
+    else:
+        print("The dealer has a blackjack, you lose")
+
+    return True
+
+def print_cards(cards, text):
     card_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-    print("Your cards:", end = " ")
-    for index in cards:
-        print(card_list[index], end = " ")
-    print()
-
-def print_dealer_cards(cards):
-    card_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-
-    print("The dealer's cards:", end = " ")
+    print(text, end = " ")
     for index in cards:
         print(card_list[index], end = " ")
     print()
 
 def player_turn(player_cards):
-    another_card = input("Type y to get another card, type n to pass: ")
+    another_card = input("Type y to get another card, type n to pass: ") # draw another card
     while another_card == "y":
         player_cards = new_card(player_cards, 1)
-        print_player_cards(player_cards)
+        print_cards(player_cards, "Your cards: ")
         if check_value(player_cards) > 21:
             print("you went over, you lost")
             return []
@@ -90,10 +86,10 @@ def dealer_turn(player_cards, dealer_cards):
     # 2. If total is >= 17, stand and compare to user
     # 3. If total is < 17, take another card and re-compute
 
-    print_dealer_cards(dealer_cards) # show both of dealers cards
+    print_cards(dealer_cards, "The dealer's cards: ") # show both of dealers cards
     while (check_value(dealer_cards)) < 17:
         dealer_cards = new_card(dealer_cards, 1)
-        print_dealer_cards(dealer_cards)
+        print_cards(dealer_cards, "The dealer's cards: ")
         if check_value(dealer_cards) > 21:
             print("The dealer went over, you won!")
             return
@@ -110,17 +106,20 @@ print(logo)
 
 player_cards = []
 player_cards = new_card(player_cards, 2)
-print_player_cards(player_cards)
+print_cards(player_cards, "Your cards: ")
 
 dealer_cards = []
 dealer_cards = new_card(dealer_cards, 2)
-print_dealer_cards(dealer_cards[:1])
+print_cards(dealer_cards[:1], "The dealer's first card: ")
 
-# # Ask user to hit
-player_cards = player_turn(player_cards)
+if not check_blackjack(player_cards, dealer_cards):
+    # # Ask user to hit
+    player_cards = player_turn(player_cards)
 
-# Dealer actions:
-if player_cards:
-    dealer_turn(player_cards, dealer_cards)
+    # Dealer actions:
+    if player_cards: # if the player cards are not empty, the game continues 
+        dealer_turn(player_cards, dealer_cards)
 
+print_cards(player_cards, "Your final cards: ")
+print_cards(dealer_cards, "The dealer's final cards: ")
 print("Thanks for playing")
