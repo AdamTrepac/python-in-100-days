eb0706beb31f061a75728226a487b0e5c3a39001
adam t <adam.trepac@gmail.com>
2024-02-04 11:24:17 -0800

    day 16 is too easy and kind of a waste of time, moving on

---
+++ Day_16/turtle_object.py
@@ -0,0 +1,15 @@
+
+
+
+from turtle import Screen, Turtle
+
+
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("green")
+
+
+print(turtle)
+my_screen = Screen()
+print(my_screen)
+my_screen.exitonclick()
