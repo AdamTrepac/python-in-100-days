e0081b967a5ed50d4640537ad1f008e917c655df
adam t <adam.trepac@gmail.com>
2024-02-04 23:49:16 -0800

    create spirograph of overalpping circles

--- Day_18/spirograph.py
+++ Day_18/spirograph.py
@@ -1,25 +1,33 @@
 """
 Have the turtle generate circles at a point that are X degrees apart in a circle
+
+Notes - radius is based on the turn angle and forward distance traveled
+    they combine to produce some radius
 """
 
 from random import randint as random_int
 from turtle import Screen, Turtle
 
-STEPSIZE = 20
-DEGREES = 100
+STEPSIZE = 5 # radius
+DEGREES = 360
+ANGLE = 4
+SPIRO_ANGLE = 30
 
 joan = Turtle("turtle")
-joan.pensize(12)
+joan.pensize(1)
 joan.speed(0)
 screen = Screen()
 screen.colormode(255)
 screen.bgcolor("black")
 
-for number_of_sides in range(100):
-    joan.left(45)
+joan.pencolor(random_int(1, 255), random_int(1, 255), random_int(1, 255))
+joan.circle(100)
+
+for _ in range(int(DEGREES/SPIRO_ANGLE)):
+    joan.left(SPIRO_ANGLE)
     joan.pencolor(random_int(1, 255), random_int(1, 255), random_int(1, 255))
-    for _ in range(number_of_sides):
-        joan.forward(10)
-        joan.right(DEGREES/number_of_sides)
+    for _ in range(int(DEGREES/ANGLE)):
+        joan.forward(STEPSIZE)
+        joan.right(ANGLE)
 
 screen.exitonclick()
