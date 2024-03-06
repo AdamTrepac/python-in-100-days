0ff9e7194a0139a94b96bf4ac13807fb7a952590
adam t <adam.trepac@gmail.com>
2024-03-05 21:24:49 -0800

    add small changes to snake movement code

--- Day_20/main.py
+++ Day_20/main.py
@@ -12,31 +12,34 @@ Snake game! There are 7 steps to the game:
 from turtle import Turtle, Screen
 from functools import partial
 
+snake_heading = 0
+
 def move_up(snake: Turtle):
-    if snake.heading() != 270:
+    if snake_heading != 270:
         snake.setheading(90)
 
 def move_down(snake: Turtle):
-    if snake.heading() != 90:
+    if snake_heading != 90:
         snake.setheading(270)
 
 def move_left(snake: Turtle):
-    if snake.heading() != 0:
+    if snake_heading != 0:
         snake.setheading(180)
 
 def move_right(snake: Turtle):
-    if snake.heading() != 180:
+    if snake_heading != 180:
         snake.setheading(0)
 
 STEP = 20
 
 def main():
-    SCREEN_WIDTH = 500
-    SCREEN_HEIGHT = 500
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
     screen = Screen()
     screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
-    screen.register_shape("mySquare", ((0, 0), (0, STEP-1), (STEP-1, STEP-1), (STEP-1, 0)))
-
+    # screen.setworldcoordinates(0, SCREEN_HEIGHT, SCREEN_WIDTH, 0)
+    screen.register_shape("mySquare", ((STEP/2, STEP/2), (-STEP/2, STEP/2), (-STEP/2, -STEP/2), (STEP/2, -STEP/2)))
+    
     # Draw the initial snake
     snake = Turtle("mySquare")
     snake_array = []
@@ -45,8 +48,13 @@ def main():
     for _ in range(3):
         snake_array.append(snake.stamp())
         snake.forward(STEP)
+    print(snake_array)
+    # generate food
+    food = Turtle("mySquare")
+    food.penup()
+    food.color("red")
 
-    snake.speed(1)
+    snake.speed(0)
     # screen.exitonclick()
     screen.onkeypress(fun = partial(move_up, snake), key = "Up")
     screen.onkeypress(fun = partial(move_down, snake), key = "Down")
@@ -54,12 +62,18 @@ def main():
     screen.onkeypress(fun = partial(move_right, snake), key = "Right")
 
     # Moving the snake
+    screen.listen()
     while True:
+        global snake_heading
+        snake_heading = snake.heading()
         snake_array.append(snake.stamp())
-        snake.forward(STEP)
+        print(snake_array[0])
         snake.clearstamp(snake_array[0])
         snake_array.pop(0)
-        screen.listen()
+        snake.forward(STEP)
+        for _ in range(10):     # delay the snake's speed
+            snake.forward(0)
+        # print(snake.position())
 
 if __name__ == "__main__":
     main()
