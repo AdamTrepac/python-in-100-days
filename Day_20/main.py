d236ba09862e104d32877de84442792b5da29074
adam t <adam.trepac@gmail.com>
2024-03-08 09:20:02 -0800

    create a food class and have food eating partially working

--- Day_20/main.py
+++ Day_20/main.py
@@ -8,7 +8,8 @@ Snake game! There are 7 steps to the game:
 6. Detect a collision with a wall
 7. Detect a collision with the tail
 """
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, Vec2D, width
+from random import randint
 
 class Snake(Turtle):
 
@@ -18,12 +19,12 @@ class Snake(Turtle):
         self.penup()
         self.color("green")
         self.snake_heading = 0
+        self.speed(0)
         self.step = step
 
     def create_snake(self):
         """Draw the initial snake of 3 pieces long
         """
-        self.speed(0)
         for _ in range(5):
             self.forward(self.step)
             self.snake_array.append((self.stamp(), self.pos()))
@@ -39,6 +40,16 @@ class Snake(Turtle):
         self.snake_array.append((self.stamp(), self.pos()))
         self.clearstamp(self.snake_array[0][0])
         self.snake_array.pop(0)
+        self.delay()
+
+    def eat_food(self):
+        """Move the snake forward, stamp at the new location, and 
+        deletes the stamp at
+        the oldest location in the array. 
+        """
+        self.snake_heading = self.heading()
+        self.forward(self.step)
+        self.snake_array.append((self.stamp(), self.pos()))
         self.delay()
 
     def check_self_collision(self):
@@ -74,11 +85,41 @@ class Snake(Turtle):
     snake_right = move_right
 
 
+class Food(Turtle):
+
+    def __init__(self, step) -> None:
+        super().__init__("mySquare")
+        self.color("Red")
+        self.penup()
+        self.speed(0)
+        self.stamp_ID = 0
+        self.step = step
+
+    def spawn_food(self, snake: Snake, width, height):
+        self.clearstamp(self.stamp_ID)
+        food_pos = Vec2D(randint(-width/2/self.step, width/2/self.step),
+            randint(-height/2/self.step, height/2/self.step))
+        food_pos *= self.step
+        for snake_seg in snake.snake_array:
+            if snake_seg[1] == food_pos:
+                food_pos = self.spawn_food(snake, width, height)
+        self.goto(food_pos)
+        self.stamp_ID = self.stamp()
+        return food_pos
+
+
 def check_wall_collision(snake: Snake, screen_width, screen_height):
     # print(abs(snake.pos()))
     if abs(snake.pos()[0]) > screen_width/2 or abs(snake.pos()[1]) > screen_height/2:
         return True
     return False
+
+def check_food_collision(snake: Snake, food: Food, screen_width, screen_height):
+    print(snake.pos(), food.pos(), snake.pos() == food.pos())
+    if snake.pos() == food.pos():
+        snake.eat_food()
+        food.spawn_food(snake, screen_width, screen_height)
+
 
 def main():
     screen_width = 600
@@ -98,6 +139,9 @@ def main():
     snake = Snake(step)
     snake.create_snake()
 
+    food = Food(step)
+    food.color("Red")
+
     # screen.exitonclick()
     screen.onkeypress(fun = snake.snake_up, key = "Up")
     screen.onkeypress(fun = snake.snake_down, key = "Down")
@@ -106,8 +150,10 @@ def main():
 
     # Moving the snake
     screen.listen()
+    food.spawn_food(snake, screen_width, screen_height)
     while game_running:
         snake.update_snake()
+        check_food_collision(snake, food, screen_width, screen_height)
         if (snake.check_self_collision() is True
                 or check_wall_collision(snake, screen_width, screen_height) is True):
             print("Collision!")
